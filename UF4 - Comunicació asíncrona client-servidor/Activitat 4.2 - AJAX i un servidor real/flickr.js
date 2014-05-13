13cd797cfa4df9ed21341eef3b65d3c1687cba12
Carlos J. García Carmona <carlosj.garcia.carmona@gmail.com>
2014-05-13 14:03:12 +0200

    Activitat 4.2 $.ajax() success

--- UF4 - Comunicació asíncrona client-servidor/Activitat 4.2 - AJAX i un servidor real/flickr.js
+++ UF4 - Comunicació asíncrona client-servidor/Activitat 4.2 - AJAX i un servidor real/flickr.js
@@ -2,6 +2,30 @@ $(document).ready(function()
 {
 	$('#loadPhotos').click(function()
 	{
-		;
+		var userId, photoTag;
+		$('#flickrPhotos').empty(); // Vacía el contenido del div contenedor de fotos
+		$(this).toggleClass('btn-primary');
+		$(this).html('Cargando');
+		$.ajax({
+			url: 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&id='+userId+'&tags='+photoTag+'&jsoncallback=?',
+			dataType: 'json',
+			complete: function(jqXHR, textStatus)
+			{
+				$('#loadPhotos').html('Cargar de nuevo');
+				$('#loadPhotos').toggleClass('btn-primary');
+			},
+			error: function(jqXHR, textStatus, errorThrown)
+			{
+                $('#error').text('Error: '+jqXHR.status+' - '+errorThrown);
+            },
+            success: function(response)
+            {
+            	$.each(response.item, function(i, item)
+            	{
+            		$('#flickrPhotos').append(imageHTML(item));
+            	});
+            }
+		})
 	});
+	function imageHTML(item)
 });
